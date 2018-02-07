65ab44d8b6ca011ba75bac5ea76d4cc775559981
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-07 22:36:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ function nowServing(katzDeliLine) {
 } else {
   var person = katzDeliLine.shift()
   return (`Currently serving ${person}.`)
+  }
 }
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return ("The line is currently empty.")
   } else {
-  } for (let i = 0; i < katzeDeliLine.length ;i++)
+     for (let i = 0; i < katzeDeliLine.length ;i++)
+  }
 }
