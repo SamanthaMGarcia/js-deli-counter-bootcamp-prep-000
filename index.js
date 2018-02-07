de6c4dd16c43b3c83d22b7e8221c11817348a991
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-07 21:54:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,3 @@ function nowServing(katzDeliLine, person) {
   person = katzDeliLine.shift()
   return (`Currently serving ${()}.`)
 }
-function currentLine(customers) {
-  if (katzDeliLine.length === 0) {
-    return ("The line is currently empty.")
-  } else 
-  
- }
