9c29d17c10cc11fd4b7abf91a71497b39c506a87
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-07 21:42:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function takeANumber(katzDeliLine, next) {
   katzDeliLine.push(next)
   return (`Welcome, ${next}. You are number ${katzDeliLine.length} in line.`)
 }
-function nowServing(katzDeliLine, first) {
+function nowServing(first) {
   if (katzDeliLine.length === 0) {
    return ("There is nobody waiting to be served!")
 } else 
