d64d24d541058b858c408a291e7e8cd90d681632
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-07 23:40:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return ("The line is currently empty.")
   } else {
-     for (let i = 0; i < katzDeliLine.length ;i++) 
+     for (let i = 0; i < katzDeliLine.length ;i++) {
       `${i + 1}. ${katzDeliLine[i]}`
+      }
   return (`The line is currently: ${i + 1}. ${katzDeliLine[i]}, ${i + 1}.${katzDeliLine[i]}, ${i + 1}.${katzDeliLine[i]}`)
     }
   }
