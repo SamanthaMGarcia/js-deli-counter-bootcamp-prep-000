aac679b7a7e6ca4f07c4f7dab5d4c7ae02a4fbad
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-07 21:49:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function nowServing(katzDeliLine) {
    return ("There is nobody waiting to be served!")
 } else 
   katzDeliLine.shift()
-  return (`Currently serving ${}.`)
+  return (`Currently serving ${()}.`)
 }
