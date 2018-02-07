c46d1b8222ed15f5963fac7b5596fbc583bf9f51
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-07 22:45:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return ("The line is currently empty.")
   } else {
-     for (let i = 0; i < katzeDeliLine.length ;i++) {
+     for (let i = 0; i < katzDeliLine.length ;i++) {
        var customer = 
-       1. ${Ada},
+       1. ${customer},
      }
   }
 }
