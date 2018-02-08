fb659a260b9f51050ec803c232d95661fb30aad4
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-08 00:10:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(katzDeliLine) {
      for (let i = 0; i < katzDeliLine.length ;i++) {
       array.push(`${i + 1}. ${katzDeliLine[i]}`)
      }
-     array.join(' . ${katzDeliLine[i]})
+     array.join(' ,' ${katzDeliLine[i]})
   return (`The line is currently: ${i + 1}. ${katzDeliLine[i]}, ${i + 1}.${katzDeliLine[i]}, ${i + 1}.${katzDeliLine[i]}`)
     }
   }
